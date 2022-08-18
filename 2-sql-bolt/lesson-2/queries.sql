925ba9291a449ccb0cf8c2e49b98cd349cdb6c34
Andres Varon <anvaron@gmail.com>
2022-08-18 05:30:08 -0400

    sql-bolt: lesson2 - completed

--- 2-sql-bolt/lesson-2/queries.sql
+++ 2-sql-bolt/lesson-2/queries.sql
@@ -1,12 +1,13 @@
---Table: Movies
+--SQL Lesson 2: Queries with constraints (Pt. 1)
 
---1: Find the title of each film 
-SELECT title FROM movies;
---2: Find the director of each film 
-SELECT director FROM movies;
---3: Find the title and director of each film
-SELECT title, director FROM movies;
---4: Find the title and year of each film
-SELECT title, year FROM movies;
---5: Find all the information about each film
-SELECT * FROM movies;
+--1: Find the movie with a row id of 6 
+SELECT * FROM movies WHERE id=6;
+
+--2: Find the movies released in the years between 2000 and 2010
+SELECT * FROM movies WHERE year BETWEEN 2000 AND 2010;
+
+--3: Find the movies not released in the years between 2000 and 2010
+SELECT * FROM movies WHERE year NOT BETWEEN 2000 AND 2010;
+
+--4: Find the first 5 Pixar movies and their release year
+SELECT title, year FROM movies WHERE id IN (1,2,3,4,5);
